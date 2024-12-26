e56a5f6e40502a105c482a6785b2bdbcaa78f333
Ryan Vu <ryanvu1131@gmail.com>
2024-12-25 16:25:34 -0800

    Created navigation bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-        <h1>Journal App</h1>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <NavBar />
+                <h1>Journal App</h1>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./components_css/NavBar.css";
+
+const NavBar = () => {
+  return (
+    <nav className='nav'>
+        <h1>
+            The Journal App
+        </h1>
+        <div className='links'>
+            <a href="/">Home</a>
+            <a href="/create">Create New Entry</a>
+            <a href="/logoff">Sign out</a>
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar
