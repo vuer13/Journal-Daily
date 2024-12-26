80d918dc3442201940c87fa39cbe5f80113be633
Ryan Vu <ryanvu1131@gmail.com>
2024-12-25 16:47:35 -0800

    Setting up routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import NavBar from './components/NavBar';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
     return (
         <Router>
             <div className="App">
                 <NavBar />
-                <h1>Journal App</h1>
+                <Routes>
+                    <Route path="/" exact component />
+                </Routes>
             </div>
         </Router>
     );
