64a3ccdc0563369550cbe4d7b8a535e4e2f8a239
Ryan Vu <ryanvu1131@gmail.com>
2025-04-17 00:41:51 -0700

    Home screen UI change

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,28 +1,37 @@
 import './Home/Home.css'
+import { Link } from 'react-router-dom';
 
-const Details = ( {journal} ) => {
+const Details = ({ journal }) => {
 
     const date = new Date(journal.createdAt);
     const formattedDate = date.toISOString().split('T')[0];
 
     return (
         <div className="details">
-            <h4 className="title">
-                {journal.title}
-            </h4>
-            <p>
-                <strong>
-                    Rating: {journal.rating}
-                </strong>
-            </p>
-            <p>
-                <strong>
-                    Summary: {journal.summary}
-                </strong>
-            </p>
-            <p className="end">
-                Created on: {formattedDate}
-            </p>
+            <div className="main">
+                <h4 className="title">
+                    {journal.title}
+                </h4>
+                <p>
+                    <strong>
+                        Rating: {journal.rating}
+                    </strong>
+                </p>
+                <p>
+                    <strong>
+                        Summary: {journal.summary}
+                    </strong>
+                </p>
+            </div>
+            <div className="right">
+                <p className="end">
+                    Created on: {formattedDate}
+                </p>
+                <div className='links'>
+                    <Link to="/delete">Delete</Link>
+                    <Link to="/view">View</Link>
+                </div>
+            </ div>
         </div>
     )
 }
